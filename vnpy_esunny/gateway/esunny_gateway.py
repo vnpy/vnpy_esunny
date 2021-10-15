5fe56b0a7be251fa9a640b60f65369c70fcc2f37
noranhe <63988546+noranhe@users.noreply.github.com>
2021-10-15 11:11:26 +0800

    Update esunny_gateway.py

--- vnpy_esunny/gateway/esunny_gateway.py
+++ vnpy_esunny/gateway/esunny_gateway.py
@@ -329,7 +329,7 @@ class QuoteApi(MdApi):
         if not data or not exchange or not commodity_info:
             return
 
-        # 只处理期货和现货    
+        # 只处理期货和现货
         if data["CommodityType"] in PRODUCT_TYPE_ES2VT:
             symbol: str = data["CommodityNo"] + data["ContractNo1"]
             product: Product = PRODUCT_TYPE_ES2VT[data["CommodityType"]]
